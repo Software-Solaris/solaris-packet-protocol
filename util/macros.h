2e09df927f320d39f9991aa9c53b01529e15de09
Silent Whisper <132281492+secureshadow@users.noreply.github.com>
2026-04-17 19:59:26 +0200

    databank: reduce pool size to 50 packets

--- util/macros.h
+++ util/macros.h
@@ -44,7 +44,7 @@
 
 /** @brief Number of packets in the static databank pool. */
 #ifndef K_SPP_DATABANK_SIZE
-#define K_SPP_DATABANK_SIZE (1000U)
+#define K_SPP_DATABANK_SIZE (50U)
 #endif
 
 /** @brief Maximum number of pub/sub subscribers that can be registered. */
