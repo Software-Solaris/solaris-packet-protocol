f5f80b2aba16d5434b44b16177539c9355f2fce2
Silent Whisper <132281492+secureshadow@users.noreply.github.com>
2026-04-30 21:03:09 +0200

    Add build/ to .gitignore; remove tracked build artifacts

--- build/CMakeFiles/spp_port.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for spp_port.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/Documents/solaris-software/solaris-v1/spp
-# Build directory: /home/user/Documents/solaris-software/solaris-v1/spp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(spp_test_core "/usr/local/bin/cgreen-runner" "/home/user/Documents/solaris-software/solaris-v1/spp/build/libspp_test_core.so")
-set_tests_properties(spp_test_core PROPERTIES  _BACKTRACE_TRIPLES "/home/user/Documents/solaris-software/solaris-v1/spp/CMakeLists.txt;118;add_test;/home/user/Documents/solaris-software/solaris-v1/spp/CMakeLists.txt;121;spp_add_test_module;/home/user/Documents/solaris-software/solaris-v1/spp/CMakeLists.txt;0;")
