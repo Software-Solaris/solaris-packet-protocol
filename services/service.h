2caba1815522d3472a16ebc7075722408729eca4
Oscar Barja <oscar.barja@alumnado.uvigo.gal>
2026-06-28 23:56:49 +0000

    added datalogger service (working with dummy producer)

--- services/service.h
+++ services/service.h
@@ -47,7 +47,7 @@ typedef struct
     spp_uint16_t tiemoutMs;      /**< Timeout for the consumer to receive a packet */
     spp_uint16_t suscribeToApid; /**< APID subscription bitmask */
     SPP_RetVal_t (*init)();      /**< Initialise the module. */
-    SPP_RetVal_t (*deliverToMailbox)(const SPP_Packet_t packet);
+    SPP_RetVal_t (*deliverToMailbox)(const SPP_Packet_t *packet);
     SPP_RetVal_t (*consumeData)(void *p_data); /**< Consume data from the module. */
 } SPP_SERVICE_ConsumerContract_t;
 
