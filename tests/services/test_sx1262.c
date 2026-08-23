283849e54402534bdf4db54e3830ff8c494adbef
Silent Whisper <132281492+secureshadow@users.noreply.github.com>
2026-08-23 20:38:13 +0200

    Added support for executing UTs without the Docker container

---
+++ tests/services/test_sx1262.c
@@ -0,0 +1,18 @@
+/**
+ * @file test_sx1262.c
+ * @brief Placeholder Cgreen test for the SX1262 driver.
+ */
+
+#include <cgreen/cgreen.h>
+
+Describe(SX1262);
+BeforeEach(SX1262)
+{
+}
+AfterEach(SX1262)
+{
+}
+
+Ensure(SX1262, placeholder)
+{
+}
