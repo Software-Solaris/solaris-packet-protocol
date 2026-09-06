9e4a3cb445c66486de445b565864c58615a06fc7
Oscar Barja <oscar.barja@alumnado.uvigo.gal>
2026-09-06 22:11:59 +0000

    comment out unused performance tracking variables

--- services/bmp390/bmp390.c
+++ services/bmp390/bmp390.c
@@ -56,17 +56,17 @@ static SPP_RetVal_t SPP_SERVICES_BMP390_getAltitude(void *p_spiHandler, float *a
 static SPP_RetVal_t SPP_SERVICES_BMP390_intEnableDrdy(void *p_spiHandler);
 
 /* Performance testing*/
-#define K_BMP390_PERFORMANCE_SAMPLES (1000U)
+#define K_BMP390_PERFORMANCE_SAMPLES (10000U)
 
-static spp_uint32_t s_bmpPerformance[K_BMP390_PERFORMANCE_SAMPLES] = {0U};
-static spp_uint32_t s_tempSpiPerformance[K_BMP390_PERFORMANCE_SAMPLES] = {0U};
-static spp_uint32_t s_pressSpiPerformance[K_BMP390_PERFORMANCE_SAMPLES] = {0U};
+// static spp_uint32_t s_bmpPerformance[K_BMP390_PERFORMANCE_SAMPLES] = {0U};
+// static spp_uint32_t s_tempSpiPerformance[K_BMP390_PERFORMANCE_SAMPLES] = {0U};
+// static spp_uint32_t s_pressSpiPerformance[K_BMP390_PERFORMANCE_SAMPLES] = {0U};
 
-static spp_uint32_t s_lastTempSpi = 0U;
-static spp_uint32_t s_lastPressSpi = 0U;
+// static spp_uint32_t s_lastTempSpi = 0U;
+// static spp_uint32_t s_lastPressSpi = 0U;
 
-static spp_uint16_t s_performanceSamples = 0U;
-static spp_bool_t s_performanceWarmupDone = false;
+// static spp_uint16_t s_performanceSamples = 0U;
+// static spp_bool_t s_performanceWarmupDone = false;
 
 /* ----------------------------------------------------------------
 * VARIABLES
@@ -89,26 +89,26 @@ const SPP_SERVICE_ProducerContract_t *SPP_SERVICES_BMP390_getProducerContract(vo
     return &g_bmp390ProducerContract;
 }
 /* Performance Test */
-spp_uint16_t SPP_SERVICES_BMP390_getPerformanceSampleCount(void)
-{
-    return s_performanceSamples;
-}
+// spp_uint16_t SPP_SERVICES_BMP390_getPerformanceSampleCount(void)
+// {
+//     return s_performanceSamples;
+// }
 
 
-SPP_RetVal_t SPP_SERVICES_BMP390_getPerformanceSample(spp_uint16_t i, spp_uint32_t *p_bmp, spp_uint32_t *p_tempSpi,
-                                                      spp_uint32_t *p_pressSpi)
-{
-    if ((i >= s_performanceSamples) || (p_bmp == NULL) || (p_tempSpi == NULL) || (p_pressSpi == NULL))
-    {
-        return K_SPP_ERROR;
-    }
+// SPP_RetVal_t SPP_SERVICES_BMP390_getPerformanceSample(spp_uint16_t i, spp_uint32_t *p_bmp, spp_uint32_t *p_tempSpi,
+//                                                       spp_uint32_t *p_pressSpi)
+// {
+//     if ((i >= s_performanceSamples) || (p_bmp == NULL) || (p_tempSpi == NULL) || (p_pressSpi == NULL))
+//     {
+//         return K_SPP_ERROR;
+//     }
 
-    *p_bmp = s_bmpPerformance[i];
-    *p_tempSpi = s_tempSpiPerformance[i];
-    *p_pressSpi = s_pressSpiPerformance[i];
+//     *p_bmp = s_bmpPerformance[i];
+//     *p_tempSpi = s_tempSpiPerformance[i];
+//     *p_pressSpi = s_pressSpiPerformance[i];
 
-    return K_SPP_OK;
-}
+//     return K_SPP_OK;
+// }
 
 /* ----------------------------------------------------------------
 * STATIC FUNCTIONS
@@ -191,12 +191,12 @@ static SPP_RetVal_t SPP_SERVICES_BMP390_acquireData(SPP_Kpid_t kpid)
         return K_SPP_ERROR;
     }
 
-    spp_uint32_t t0 = SPP_HAL_TIME_getTimeUs();
+    // spp_uint32_t t0 = SPP_HAL_TIME_getTimeUs();
 
     SPP_RetVal_t ret =
         SPP_SERVICES_BMP390_getAltitude(p_bmpData->spiConfig.p_spiHandler, &altitude, &pressure, &temperature);
 
-    spp_uint32_t t1 = SPP_HAL_TIME_getTimeUs();
+    // spp_uint32_t t1 = SPP_HAL_TIME_getTimeUs();
 
     if (ret != K_SPP_OK)
     {
@@ -205,18 +205,18 @@ static SPP_RetVal_t SPP_SERVICES_BMP390_acquireData(SPP_Kpid_t kpid)
         return ret;
     }
 
-    if (s_performanceWarmupDone == false)
-    {
-        s_performanceWarmupDone = true;
-    }
-    else if (s_performanceSamples < K_BMP390_PERFORMANCE_SAMPLES)
-    {
-        s_bmpPerformance[s_performanceSamples] = t1 - t0;
-        s_tempSpiPerformance[s_performanceSamples] = s_lastTempSpi;
-        s_pressSpiPerformance[s_performanceSamples] = s_lastPressSpi;
+    // if (s_performanceWarmupDone == false)
+    // {
+    //     s_performanceWarmupDone = true;
+    // }
+    // else if (s_performanceSamples < K_BMP390_PERFORMANCE_SAMPLES)
+    // {
+    //     s_bmpPerformance[s_performanceSamples] = t1 - t0;
+    //     s_tempSpiPerformance[s_performanceSamples] = s_lastTempSpi;
+    //     s_pressSpiPerformance[s_performanceSamples] = s_lastPressSpi;
 
-        s_performanceSamples++;
-    }
+    //     s_performanceSamples++;
+    // }
 
 #ifdef SPP_DEBUG_PRINT
     SPP_LOGI(K_BMP390_TAG, "[BMP] alt=%.1fm P=%.1fhPa T=%.2fC\n", altitude, pressure / 100.0f, temperature);
@@ -432,11 +432,11 @@ static SPP_RetVal_t SPP_SERVICES_BMP390_readRawTemp(void *p_spiHandler, uint32_t
         (spp_uint8_t)(K_BMP390_SPI_READ | (K_BMP390_TEMP_RAW_REG + 1)), K_BMP390_SPI_WRITE, K_BMP390_SPI_WRITE,
         (spp_uint8_t)(K_BMP390_SPI_READ | (K_BMP390_TEMP_RAW_REG + 2)), K_BMP390_SPI_WRITE, K_BMP390_SPI_WRITE};
 
-    spp_uint32_t t0 = SPP_HAL_TIME_getTimeUs();
+    // spp_uint32_t t0 = SPP_HAL_TIME_getTimeUs();
     SPP_RetVal_t ret = SPP_HAL_SPI_transmit(p_spiHandler, buf, sizeof(buf));
-    spp_uint32_t t1 = SPP_HAL_TIME_getTimeUs();
+    // spp_uint32_t t1 = SPP_HAL_TIME_getTimeUs();
 
-    s_lastTempSpi = t1 - t0;
+    // s_lastTempSpi = t1 - t0;
 
     if (ret != K_SPP_OK)
     {
@@ -621,11 +621,11 @@ static SPP_RetVal_t SPP_SERVICES_BMP390_readRawPress(void *p_spiHandler, spp_uin
         (spp_uint8_t)(K_BMP390_SPI_READ | (K_BMP390_PRESS_RAW_REG + 1)), K_BMP390_SPI_WRITE, K_BMP390_SPI_WRITE,
         (spp_uint8_t)(K_BMP390_SPI_READ | (K_BMP390_PRESS_RAW_REG + 2)), K_BMP390_SPI_WRITE, K_BMP390_SPI_WRITE};
 
-    spp_uint32_t t0 = SPP_HAL_TIME_getTimeUs();
+    // spp_uint32_t t0 = SPP_HAL_TIME_getTimeUs();
     SPP_RetVal_t ret = SPP_HAL_SPI_transmit(p_spiHandler, buf, sizeof(buf));
-    spp_uint32_t t1 = SPP_HAL_TIME_getTimeUs();
+    // spp_uint32_t t1 = SPP_HAL_TIME_getTimeUs();
 
-    s_lastPressSpi = t1 - t0;
+    // s_lastPressSpi = t1 - t0;
 
     if (ret != K_SPP_OK)
     {
